8a610d33796290e2ce15faf616617ad095cb5bf3
Ulukman Zhamshitov <uakmatof@gmail.com>
2021-03-11 13:51:49 -0600

    first commit

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+# 1. TASK: print "Hello World"
+print( 'Hello World');
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle";
+print( 'Hello', name );	# with a comma
+print( 'Hello ' + name );	# with a +
+
+# 3. print "Hello 42!" with the number in a variable
+num = 42;
+print( 'Hello', num);
+# print( 'Hello ' + num); error
+print( 'Hello ' + str(num)); #casting int to string
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = 'suhsi';
+fave_food2 = 'pizza';
+print( f'I love eat {fave_food1} and {fave_food2}');
+print('I love eat {} and {}'.format(fave_food1,fave_food2));
+
+
+
+
+
